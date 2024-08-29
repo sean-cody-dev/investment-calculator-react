905a60f502f9236ebcc14c633fa64bb86c078d84
sean-cody-dev <meghancody@Meghans-MacBook.local>
2024-08-29 13:33:20 -0400

    initial project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,68 @@
 import { useState } from "react";
+import { calculateInvestmentResults, formatter } from "./util/investment";
+
+import Row from "./Row";
 
 function App() {
-
-  const [ initialInvestment, setInitialInvestment ] = useState(null);
-  const [ expectedReturn, setExpectedReturn ] = useState(null);
-  const [ annualInvestment, setAnnualInvestment ] = useState(null);
-  const [ duration, setDuration] = useState(null);
+  const [initialInvestment, setInitialInvestment] = useState(0);
+  const [expectedReturn, setExpectedReturn] = useState(0);
+  const [annualInvestment, setAnnualInvestment] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   function handleInitialInvestment(event) {
-    setInitialInvestment(event.target.value);
+    setInitialInvestment(Number(event.target.value));
   }
 
-  function handleExpectedReturn(event){
+  function handleExpectedReturn(event) {
     setExpectedReturn(event.target.value);
   }
 
   function handleAnnualInvestment(event) {
-    setAnnualInvestment(event.target.value);
+    setAnnualInvestment(Number(event.target.value));
   }
 
-  function handleDuration(event){
-    setDuration(event.target.value);
+  function handleDuration(event) {
+    setDuration(Number(event.target.value));
   }
+
+  // Passes state values to investment calculator
+  const annualData = calculateInvestmentResults({
+    initialInvestment,
+    annualInvestment,
+    expectedReturn,
+    duration,
+  });
+
+  // initialize sums as zero
+  let totalInterest = 0;
+  let investedCapital = 0;
+
+  // for each item in annualData, create a row for results
+  const resultsTable = (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* map annualData here */}
+        {annualData.map((rowData) => {
+          totalInterest += rowData.interest;
+          investedCapital = initialInvestment + annualInvestment * rowData.year;
+          return <Row 
+            rowData={rowData} 
+            totalInterest={totalInterest}
+            investedCapital={investedCapital}
+          />;
+        })}
+      </tbody>
+    </table>
+  );
 
   return (
     <>
@@ -32,17 +73,38 @@ function App() {
       <div id="user-input" className="input-group">
         <div>
           <label htmlFor="initialInvestment">Initial Investment</label>
-          <input type="number" name="initialInvestment" id="initialInvestment" onChange={handleInitialInvestment}/>
+          <input
+            type="number"
+            name="initialInvestment"
+            id="initialInvestment"
+            onChange={handleInitialInvestment}
+          />
           <label htmlFor="expectedReturn">Expected Return</label>
-          <input type="number" name="expectedReturn" id="expectedReturn" onChange={handleExpectedReturn}/>
+          <input
+            type="number"
+            name="expectedReturn"
+            id="expectedReturn"
+            onChange={handleExpectedReturn}
+          />
         </div>
         <div>
           <label htmlFor="annualInvestment">Annual Investment</label>
-          <input type="number" name="annualInvestment" id="annualInvestment" onChange={handleAnnualInvestment}/>
+          <input
+            type="number"
+            name="annualInvestment"
+            id="annualInvestment"
+            onChange={handleAnnualInvestment}
+          />
           <label htmlFor="duration">Duration</label>
-          <input type="number" name="duration" id="duration" onChange={handleDuration}/>
+          <input
+            type="number"
+            name="duration"
+            id="duration"
+            onChange={handleDuration}
+          />
         </div>
       </div>
+      {resultsTable}
     </>
   );
 }
---
+++ src/Row.jsx
@@ -0,0 +1,16 @@
+import { formatter } from "./util/investment";
+
+export default function Row({rowData, totalInterest, investedCapital}) {
+
+    const { year, valueEndOfYear, interest } = rowData;
+
+    return (
+        <tr>
+        <td>{year}</td>
+        <td>{formatter.format(valueEndOfYear)}</td>
+        <td>{formatter.format(interest)}</td>
+        <td>{formatter.format(totalInterest)}</td>
+        <td>{formatter.format(investedCapital)}</td>
+      </tr>
+    )
+}
